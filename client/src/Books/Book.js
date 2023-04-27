1cfdb7696515e5ee817823a0fc8b05d504cc93f4
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-04-28 02:11:07 +0800

    starting code

---
+++ client/src/Books/Book.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Book.css'
+
+function Book(){
+    return(
+        <div>
+            <h1>Book1</h1>
+        </div>
+    )
+}
+
+export default Book;
---
+++ client/src/Books/BookList.js
@@ -0,0 +1,13 @@
+import Book from "./Book";
+import './BookList.css'
+
+
+function BookList(){
+    return (
+        <div>
+            <Book/>
+        </div>
+    )
+}
+
+export default BookList;
