986bfb4ef34b3a3c19f920f415a008389dcaeaf5
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-04-30 01:21:01 +0800

    added add book form

--- client/src/pages/AddBook.js
+++ client/src/pages/AddBook.js
@@ -1,7 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function AddBook(){
     return (
-        <div>
-            <h1>Add Book</h1>
+        <div style={{display:'block',width: 700,padding:30}}>
+            <h4>Add Book</h4>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Book Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter book title" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Publisher</Form.Label>
+                    <Form.Control type="text" placeholder="Enter publisher" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Year</Form.Label>
+                    <Form.Control type="date" placeholder="Enter year book was published" />
+                    <Form.Text className="text-muted">
+                        The year must be in YYYY format.
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add Book
+                </Button>
+            </Form>
         </div>
     );
 }
